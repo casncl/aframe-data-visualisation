fb6e54e26ae86afc711d732d592057296bb2a73f
ChrGarske <30832694+ChrGarske@users.noreply.github.com>
2019-04-25 15:10:29 +0100

    created visualization with aframe entities instead of svg

---
+++ plot.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent('plot',{
+    schema:{
+        xtitle: {default:''},
+        xlabels: {default:[]},
+        xbreaks: {default:[]},
+        ytitle: {default:''},
+        ylabels: {default:[]},
+        ybreaks: {default:[]},
+        ztitle: {default:''},
+        zlabels: {default:[]},
+        zbreaks: {default:[]}
+    },
+    init: function(){
+        
+    },
+    update: function() {
+
+    }
+})
