d466da20200797d7bd5e2b5a1a56a6511216b8ef
ChrGarske <30832694+ChrGarske@users.noreply.github.com>
2019-05-09 14:38:00 +0100

    dd inc raycast

--- aframe-scatterplot.js
+++ aframe-scatterplot.js
@@ -414,8 +414,3 @@ AFRAME.registerComponent('tiles', {
         };
     }
 });
-// window.addEventListener("gamepadconnected", function(e) {
-//     console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-//       e.gamepad.index, e.gamepad.id,
-//       e.gamepad.buttons.length, e.gamepad.axes.length);
-//   });
